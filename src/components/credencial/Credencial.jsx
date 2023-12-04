60b744caac3de0ee93a616541d33bf375c6232fa
Emmily Galvan <emmilygalvan11@gmail.com>
2023-12-04 12:12:54 -0600

    credencial

--- src/components/credencial/Credencial.jsx
+++ src/components/credencial/Credencial.jsx
@@ -6,12 +6,21 @@ export const Credencial = () => {
     <div className={Styles.conteiner}>
       <div className={Styles.crendencial}>
         <div className={Styles.header}>
-            <img className={Styles.logo} src={logo} alt="logo" onClick={() => {
-            navigate('/')
-            }} />
-
-            <p>Sindicato de Trabajadores a Servicio de los Poderes del Estado</p>
-            <p>Comité Ejecutivo 2023-2026</p>
+            <img className={Styles.logo} src={logo} alt="logo"/>
+            <div className={Styles.stspe}> 
+                <p className={Styles.stspeName}><b>Sindicato de Trabajadores a Servicio de los Poderes del Estado</b></p>
+                <p className={Styles.comite}>Comité Ejecutivo 2023-2026</p>
+            </div>
+        </div>
+        <div className={Styles.body}>
+            <div className={Styles.nameandLogo}>
+                <div className={Styles.name}>
+                    <p>Nombre</p>
+                    <p>Apellido</p>
+                    <p>Apellido</p>
+                </div>
+                <img className={Styles.logoFondo} src={logo} alt="logo"/>        
+            </div>
         </div>
       </div>
     </div>
