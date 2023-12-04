7b31d7be329a58c9751ee5b9d5dc3e8bc8a033d9
Emmily Galvan <emmilygalvan11@gmail.com>
2023-12-03 20:30:04 -0600

    no se que, pero me robe tu codigo de employeeDetail

--- src/Pages/EmployeeDetail.jsx
+++ src/Pages/EmployeeDetail.jsx
@@ -1,28 +1,33 @@
-import { Flex } from "@chakra-ui/react";
-import { useLocation, useNavigate } from "react-router-dom"
+import { Flex } from '@chakra-ui/react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import back from '../assets/back.svg';
 
-import QRCode from "react-qr-code";
+import QRCode from 'react-qr-code';
 
 export const EmployeeDetail = () => {
-
     const { state } = useLocation();
+
     const navigate = useNavigate();
-    const { id, nombre, apellidop, apellidom, } = state;
-    return (
-    <div
-        style={{
-            margin: '90px 40px 40px 120px',
-            display: 'flex',
-            height: '100vh',
-            width: '1300px'
-        }}
-    >
-        <div>
+
+    const { id, nombre, apellidop, apellidom, fotoempleado } = state;
+
+
+  return (
+    <div className={Styles.container}>
+        <div className={Styles.screenName}>
+            <img src={back} alt="back" className={Styles.back} onClick={() => {
+                navigate('/empleados')
+                }}
+            />
+            <p className={Styles.screenTitle}>  {nombre} </p>
+        </div>
+
+        <div className={Styles.container}>
             <QRCode value={id} />
         </div>
         <h2>
             {
-                nombre
+                
             }
         </h2>
         <h2>
@@ -30,81 +35,41 @@ export const EmployeeDetail = () => {
                 apellidop
             }
         </h2>
-        <h2>
-            {
-                apellidom
-            }
-        </h2>
-        <button style={{
-            background:'#FF8000',
-            border: 'none',
-            height: '32px',
-            width:'280px',
-            colo:'white',
-        }}
-            onClick={() => {
-                navigate('/editEmpleado', {
-                    state
-                })
-            }}
-        >
-            Editar
-        </button>
-    </div>
-  )
-}
-import { Flex } from "@chakra-ui/react";
-import { useLocation, useNavigate } from "react-router-dom"
 
-import QRCode from "react-qr-code";
+            <div>
+                <img src={back} alt="back" />
+                <p>Registrar Sindicalizado</p>
+            </div>
 
-export const EmployeeDetail = () => {
-
-    const { state } = useLocation();
-    const navigate = useNavigate();
-    const { id, nombre, apellidop, apellidom, } = state;
-    return (
-    <div
-        style={{
-            margin: '90px 40px 40px 120px',
-            display: 'flex',
-            height: '100vh',
-            width: '1300px'
-        }}
-    >
-        <div>
-            <QRCode value={id} />
+            <div>
+                <QRCode value={id} />
+            </div>
+            <h2>{nombre}</h2>
+            <h2>{apellidop}</h2>
+            <h2>{apellidom}</h2>
+            <img src={
+                `./images/${fotoempleado}`
+            } alt="foto" />
+            <p>
+                {fotoempleado}
+            </p>
+            <button
+                style={{
+                    background: '#FF8000',
+                    border: 'none',
+                    height: '32px',
+                    width: '280px',
+                    colo: 'white',
+                }}
+                onClick={() => {
+                    navigate('/editEmpleado', {
+                        state,
+                    });
+                }}
+            >
+                Editar
+            </button>
         </div>
-        <h2>
-            {
-                nombre
-            }
-        </h2>
-        <h2>
-            {
-                apellidop
-            }
-        </h2>
-        <h2>
-            {
-                apellidom
-            }
-        </h2>
-        <button style={{
-            background:'#FF8000',
-            border: 'none',
-            height: '32px',
-            width:'280px',
-            colo:'white',
-        }}
-            onClick={() => {
-                navigate('/editEmpleado', {
-                    state
-                })
-            }}
-        >
-            Editar
-        </button>
-    </div>
-  )
-}
+    );
+};
+
